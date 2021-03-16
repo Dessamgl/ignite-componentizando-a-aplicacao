25cf7b868b5a006b404241af1b2e0d441223ba14
Dessamgl <dessa1413@gmail.com>
2021-03-16 03:20:48 -0300

    Challenge completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from 'react-dom'
+import { MovieCatalogProvider } from './contexts/MovieCatalogContext';
 
 import { App } from './App'
 
-render(<App />, document.getElementById('root'))
+render(
+  <MovieCatalogProvider>
+    <App />
+  </MovieCatalogProvider>, 
+  document.getElementById('root')
+)
